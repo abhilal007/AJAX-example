e4ea50742b71e1aa50abb4ec17a46fb65cfc008f
MaskyS <kufahmeeran@gmail.com>
2017-12-14 13:51:49 +0400

    Update AjaxExampleDynamicSectionsDegardes.php
    Replace instances of t() with $this->t()

--- ajax_example/src/Form/AjaxExampleDynamicSectionsDegardes.php
+++ ajax_example/src/Form/AjaxExampleDynamicSectionsDegardes.php
@@ -58,7 +58,7 @@ class AjaxExampleDynamicSectionsDegardes extends FormBase {
     ];
     $form['question_type_select'] = [
       '#type' => 'select',
-      '#title' => t('Question style'),
+      '#title' => $this->t('Question style'),
       '#options' => [
         'Choose question style' => 'Choose question style',
         'Multiple Choice' => 'Multiple Choice',
@@ -74,7 +74,7 @@ class AjaxExampleDynamicSectionsDegardes extends FormBase {
     // The CSS for this module hides this next button if JS is enabled.
     $form['question_type_submit'] = [
       '#type' => 'submit',
-      '#value' => t('Choose'),
+      '#value' => $this->t('Choose'),
       '#attributes' => ['class' => ['next-button']],
     // No need to validate when submitting this.
       '#limit_validation_errors' => [],
@@ -101,7 +101,7 @@ class AjaxExampleDynamicSectionsDegardes extends FormBase {
     if (!empty($form_state->getValue('question_type_select'))) {
 
       $form['questions_fieldset']['question'] = [
-        '#markup' => t('Who was the first president of the U.S.?'),
+        '#markup' => $this->t('Who was the first president of the U.S.?'),
       ];
       $question_type = $form_state->getValue('question_type_select');
 
@@ -109,7 +109,7 @@ class AjaxExampleDynamicSectionsDegardes extends FormBase {
         case 'Multiple Choice':
           $form['questions_fieldset']['question'] = [
             '#type' => 'radios',
-            '#title' => t('Who was the first president of the United States'),
+            '#title' => $this->t('Who was the first president of the United States'),
             '#options' => [
               'George Bush' => 'George Bush',
               'Adam McGuire' => 'Adam McGuire',
